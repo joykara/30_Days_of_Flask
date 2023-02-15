a4febcab0ffa81491947cc16587019a736d38f16
joykara <joy.karani1@gmail.com>
2023-02-15 16:51:09 +0300

    import db model

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -6,3 +7,6 @@ class Config(object):
     #value is an  expression with two terms, joined by the 'or'
     #first term, environment variable value
     #second term, is just a hardcoded string
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
