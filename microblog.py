ed600887389a95dc341c9fd6d6ca033bc9aa18c5
joykara <joykarani1@gmail.com>
2023-02-16 20:30:41 +0300

    update microblog.py

--- microblog.py
+++ microblog.py
@@ -1,3 +1,7 @@
 from app import app  #import app variable from app pckg
+from app.models import User, Post
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
