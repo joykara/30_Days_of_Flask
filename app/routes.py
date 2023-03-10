72f1538f00c463d66320fa8da24ce728515771f5
joykara <joykarani1@gmail.com>
2023-03-10 22:21:51 +0300

    Error handlers

--- app/routes.py
+++ app/routes.py
@@ -108,4 +108,4 @@ def edit_profile():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080', debug=True)
-    #debug mode: set FLASK_ENV=development
+    #debug mode: set FLASK_DEBUG=development
